3b77c144171a288403c93689c093422f68a74f77
GuilhermeHenriqueMelo <guilherme.melo0500@gmail.com>
2019-02-24 22:10:54 -0300

    Finished the tutorial: 'Tour of Heroes' from Angular.io

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
 
 import { HeroDetailComponent } from './hero-detail.component';
 
@@ -8,7 +10,8 @@ describe('HeroDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroDetailComponent ]
+      declarations: [ HeroDetailComponent ],
+      imports: [RouterTestingModule, FormsModule]
     })
     .compileComponents();
   }));
